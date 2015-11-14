0c7d40d9259ef71f437b484f22d6ae6c0e06c89f
Eric Normandeau <eric.normandeau.qc@gmail.com>
2015-11-14 13:01:03 -0500

    Add planet.sh and data folder with earth

---
+++ planets.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Displays information about the planets of the solar system
+
+# List the planets
+echo "The planets of the solar system are:"
+ls -1 data
+
+# Print informations about the planets
+echo "The description of the earth is:"
+cat data/earth
+
